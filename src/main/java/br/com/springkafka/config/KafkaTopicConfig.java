17af4beb1d5163d72b71386c7d2feffcf8eff6bb
Wa10 <walace.alves10@gmail.com>
2025-07-19 22:03:51 -0300

    add create topic

---
+++ src/main/java/br/com/springkafka/config/KafkaTopicConfig.java
@@ -0,0 +1,19 @@
+package br.com.springkafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaTopicConfig {
+
+    @Value("${topic.name}")
+    private String topicName;
+
+    @Bean
+    public NewTopic createTopic() {
+        return new NewTopic(topicName, 1, (short) 1);
+    }
+
+}
